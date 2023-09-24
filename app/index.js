c8a3560aca4de32eadc550b345383ed4676702d8
krishimodh <krishimodh@gmail.com>
2023-09-24 20:16:02 +0530

    feat: :lipstick: build structre
    add assets , components , constants ,style, app folders

---
+++ app/index.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { View, ScrollView , SafeAreaView} from 'react-native';
+import { Stack,useRouter } from 'expo-router';
+import { COLORS,images, icons, SIZE } from '../constants'; 
+const Home= ()=> {
+      return (
+        <View>
+            <Text>
+                Wool Mart
+            </Text>
+        </View>
+      );
+}
+export default Home;
+
