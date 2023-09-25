f57cedb5f2289a2d47f3d4465f4f060aa115af94
krishimodh <krishimodh@gmail.com>
2023-09-25 15:15:56 +0530

    feat: :lipstick: upadate home page
    Update screenHeader , PopularJob, Welcome components
    
    update in home page

--- app/index.js
+++ app/index.js
@@ -1,15 +1,43 @@
-import { useState } from 'react';
-import { View, ScrollView , SafeAreaView} from 'react-native';
-import { Stack,useRouter } from 'expo-router';
-import { COLORS,images, icons, SIZE } from '../constants'; 
-const Home= ()=> {
-      return (
-        <View>
-            <Text>
-                Wool Mart
-            </Text>
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, images, icons, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex:1,
+            padding:SIZES.medium
+          }}
+        >
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
         </View>
-      );
-}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 export default Home;
-
